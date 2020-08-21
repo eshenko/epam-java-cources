0c795363228245602c53b6d9e7d1eda81b9ce5d3
Alexey Eshenko <alexeyeschenko@gmail.com>
2020-08-21 19:58:48 +0300

    Add task006 realization

---
+++ src/main/java/com/epam/university/java/core/task006/Task006Impl.java
@@ -0,0 +1,34 @@
+package com.epam.university.java.core.task006;
+
+import java.util.Collection;
+
+public class Task006Impl implements Task006 {
+    @Override
+    public double resistance(Collection<Measurement> measurements) {
+        if (measurements == null) {
+            throw new IllegalArgumentException();
+        }
+
+        double avgU = 0.0;
+        double avgI = 0.0;
+        double devU = 0.0;
+        double devI = 0.0;
+
+        for (Measurement m : measurements) {
+            avgU += m.getVoltage();
+            avgI += m.getAmperage();
+        }
+
+        avgU /= measurements.size();
+        avgI /= measurements.size();
+
+        //R=(U*I)/(I*I)
+
+        for (Measurement m : measurements) {
+            devU += (m.getVoltage() - avgU) * (m.getAmperage() - avgI);
+            devI += Math.pow(m.getAmperage() - avgI, 2);
+        }
+
+        return ((int)((devU / devI) * 1000)) / 1000.0;
+    }
+}

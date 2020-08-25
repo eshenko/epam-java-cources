0cecc137a79772e1ab98e38328c77b7dd13782a8
eshenko <alexeyeschenko@gmail.com>
2020-08-25 17:39:39 +0300

    Add task009 realization

---
+++ src/main/java/com/epam/university/java/core/task009/Task009Impl.java
@@ -0,0 +1,31 @@
+package com.epam.university.java.core.task009;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Task009Impl implements Task009 {
+    @Override
+    public Collection<String> countWords(File sourceFile) {
+        StringBuilder text = new StringBuilder();
+        String line = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFile))) {
+            while ((line = br.readLine()) != null) {
+                text.append("\n").append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String[] words = text.toString()
+                .toLowerCase()
+                .trim()
+                .replaceAll("[^a-z\\-\\s]", "")
+                .split("\\s+");
+        return new HashSet<>(Arrays.asList(words));
+    }
+}

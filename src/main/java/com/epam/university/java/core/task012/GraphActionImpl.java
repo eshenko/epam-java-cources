0b2e863bb306e64867873dacc6f7bd2ca5aedd90
eshenko <alexeyeschenko@gmail.com>
2020-08-27 00:37:08 +0300

    Add task012 realization

---
+++ src/main/java/com/epam/university/java/core/task012/GraphActionImpl.java
@@ -0,0 +1,6 @@
+package com.epam.university.java.core.task012;
+
+public class GraphActionImpl implements GraphAction {
+    @Override
+    public void run(Graph graph) {}
+}
---
+++ src/main/java/com/epam/university/java/core/task012/GraphImpl.java
@@ -0,0 +1,42 @@
+package com.epam.university.java.core.task012;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class GraphImpl implements Graph {
+    private final int vertexes;
+    private final boolean[][] edges;
+
+    public GraphImpl(int vertexesCount) {
+        this.vertexes = vertexesCount + 1;
+        this.edges = new boolean[vertexes][vertexes];
+    }
+
+    @Override
+    public void createEdge(int from, int to) {
+        edges[from][to] = true;
+        edges[to][from] = true;
+    }
+
+    @Override
+    public boolean edgeExists(int from, int to) {
+        return edges[from][to];
+    }
+
+    @Override
+    public void removeEdge(int from, int to) {
+        edges[from][to] = false;
+        edges[to][from] = false;
+    }
+
+    @Override
+    public Collection<Integer> getAdjacent(int from) {
+        Collection<Integer> adjacent = new ArrayList<>();
+        for (int i = 0; i < vertexes; i++) {
+            if (edges[from][i]) {
+                adjacent.add(i);
+            }
+        }
+        return adjacent;
+    }
+}

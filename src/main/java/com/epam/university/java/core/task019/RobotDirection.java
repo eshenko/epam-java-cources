3730f2073e7ccc62c331be7b6606439b60d1f964
eshenko <alexeyeschenko@gmail.com>
2020-09-04 09:21:54 +0300

    Add task019 realization

---
+++ src/main/java/com/epam/university/java/core/task019/RobotDirection.java
@@ -0,0 +1,23 @@
+package com.epam.university.java.core.task019;
+
+/**
+ * Robot directions.
+ */
+public enum RobotDirection {
+    /**
+     * left.
+     */
+    LEFT,
+    /**
+     * right.
+     */
+    RIGHT,
+    /**
+     * up.
+     */
+    UP,
+    /**
+     * down.
+     */
+    DOWN
+}
